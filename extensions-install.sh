d197a653de1bf58b8c3051dd2633764e502442e8
Christian O. Häusler <der.haeusler@gmx.net>
2025-06-06 22:46:48 +0200

    fix: update installed extensions

--- extensions-install.sh
+++ extensions-install.sh
@@ -5,30 +5,36 @@
 # sudo apt install /home/{username}/vscode-current.deb
 
 # Install vscode extensions
-code --install-extension blackboxapp.blackbox@2.8.32
-code --install-extension blackboxapp.blackboxagent@3.1.39
+code --install-extension blackboxapp.blackbox@2.8.43
+code --install-extension blackboxapp.blackboxagent@3.2.92
 code --install-extension charliermarsh.ruff@2025.22.0
 code --install-extension christian-kohler.path-intellisense@2.10.0
-code --install-extension docker.docker@0.4.6
+code --install-extension docker.docker@0.8.1
 code --install-extension donjayamanne.githistory@0.6.20
+code --install-extension esbenp.prettier-vscode@11.0.0
 code --install-extension frhtylcn.pythonsnippets@1.0.2
+code --install-extension github.github-vscode-theme@6.3.5
 code --install-extension mhutchie.git-graph@1.30.0
-code --install-extension ms-azuretools.vscode-docker@1.29.5
+code --install-extension ms-azuretools.vscode-containers@2.0.3
+code --install-extension ms-azuretools.vscode-docker@2.0.0
 code --install-extension ms-python.autopep8@2025.2.0
 code --install-extension ms-python.black-formatter@2025.2.0
-code --install-extension ms-python.debugpy@2025.6.0
+code --install-extension ms-python.debugpy@2025.8.0
 code --install-extension ms-python.flake8@2025.2.0
 code --install-extension ms-python.isort@2025.0.0
 code --install-extension ms-python.mypy-type-checker@2025.2.0
 code --install-extension ms-python.pylint@2025.2.0
-code --install-extension ms-python.python@2025.4.0
-code --install-extension ms-python.vscode-pylance@2025.4.1
-code --install-extension ms-toolsai.jupyter@2025.3.0
+code --install-extension ms-python.python@2025.6.1
+code --install-extension ms-python.vscode-pylance@2025.5.1
+code --install-extension ms-toolsai.jupyter@2025.4.1
 code --install-extension ms-toolsai.jupyter-keymap@1.1.2
 code --install-extension ms-toolsai.jupyter-renderers@1.1.0
 code --install-extension ms-toolsai.vscode-jupyter-cell-tags@0.1.9
 code --install-extension ms-toolsai.vscode-jupyter-slideshow@0.1.6
-code --install-extension ms-vscode-remote.remote-containers@0.409.0
+code --install-extension ms-vscode-remote.remote-containers@0.413.0
 code --install-extension njpwerner.autodocstring@0.6.1
-code --install-extension pkief.material-icon-theme@5.21.2
-code --install-extension vscode-icons-team.vscode-icons@12.12.0
+code --install-extension pkief.material-icon-theme@5.23.0
+code --install-extension redhat.vscode-yaml@1.18.0
+code --install-extension tamasfe.even-better-toml@0.21.2
+code --install-extension vscode-icons-team.vscode-icons@12.13.0
+code --install-extension yzane.markdown-pdf@1.5.0
